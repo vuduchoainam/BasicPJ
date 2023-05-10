7c1865815f1e2c86a12f6bcf25de10146a9c482d
vuduchoainam <vinhoainam0815@gmail.com>
2023-05-10 21:27:58 +0700

    create area

---
+++ BasicPJ/Areas/Admin/Controller/ManageUserController.cs
@@ -0,0 +1,21 @@
+﻿using BasicPJ.Controllers;
+using BasicPJ.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicPJ.Areas.Admin.Controller
+{
+    [Authorize(Roles = "Admin")]
+    public class ManageUserController : BaseController<ApplicationUser>
+    {
+        // GET: Admin/ManageUser
+        public ActionResult Index()
+        {
+            var users = GetAll().ToList();
+            return View(users);
+        }
+    }
+}
